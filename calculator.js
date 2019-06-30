0b72353f213d39c4b0227911f6006d4deda1903c
Marina Hokkonen <mariskasl@mail.ru>
2019-06-30 18:02:26 +0300

    simple calculator

---
+++ calculator.js
@@ -0,0 +1,7 @@
+exports.add = (number1 = 0, number2 = 0 ) => Number(number1) + Number(number2);
+
+exports.subtract = (number1 = 0, number2 = 0) => number1 - number2;
+
+exports.multiply = (number1 = 0, number2 = 0) => number1 * number2;
+
+exports.division = (number1 = 0, number2 = 0) => number1 / number2;
